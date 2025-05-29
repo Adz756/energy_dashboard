ea655151908ff414904dfe852123e390cf583fb7
Adz756 <63112148+Adz756@users.noreply.github.com>
2025-05-29 11:39:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,42 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file, parse_dates=["Timestamp"])
     st.write("Dane źródłowe:", df.head())
 
+    df = df.sort_values("Timestamp")
+    #df["delta_t_s"] = df["Timestamp"].diff().dt.total_seconds()
+    #df = df.dropna(subset=["delta_t_s"])
+    df["delta_t_s"] = 300
+    df["date"] = df["Timestamp"].dt.date
+    df["Output Active Energy"] = (df["Output Active Power(W)"] * (df["delta_t_s"] / 3600.0))/1000
+    df["Battery Charge Energy"] = ((df["Battery Power(W)"] * (df["delta_t_s"] / 3600.0))/1000).where(df["Battery Power(W)"] > 0, 0)
+    df["Battery Discharge Energy"] = ((df["Battery Power(W)"] * (df["delta_t_s"] / 3600.0))/1000).where(df["Battery Power(W)"] < 0, 0)
+    df["PV Production"] = (df["PV Power(W)"] * (df["delta_t_s"] / 3600.0))/1000
+    df["PV Charge Energy"] = (df["PV Charge Power(W)"] * (df["delta_t_s"] / 3600.0))/1000
+    df["Grid Import Energy"] = ((df["Grid Power(W)"] * (df["delta_t_s"] / 3600.0))/1000).where(df["Grid Power(W)"] > 0, 0)
+    df["Grid Export Energy"] = ((df["Grid Power(W)"] * (df["delta_t_s"] / 3600.0))/1000).where(df["Grid Power(W)"] < 0, 0)
+
+    day_energy = df.groupby("date")[["PV Production","Output Active Energy","Battery Charge Energy","Grid Import Energy","Grid Export Energy"]].sum().reset_index()
+
+    pv_energy = df["PV Production"].sum()
+    output_energy = df["Output Active Energy"].sum()
+    pv_charge_energy = df["PV Charge Energy"].sum()
+    batt_charge_energy = df["Battery Charge Energy"].sum()
+    batt_discharge_energy = df["Battery Discharge Energy"].sum()
+    grid_import_energy = df["Grid Import Energy"].sum()
+    grid_export_energy = df["Grid Export Energy"].sum()
+
+    st.write(f"PV production: {pv_energy:.1f} kWh")
+    st.write(f"Grid import: {grid_import_energy:.1f} kWh")
+    st.write(f"Output: {output_energy:.1f} kWh")
+    #st.write(f"PV Charge: {pv_charge_energy:.1f} kWh")
+    st.write(f"Batt Charge: {batt_charge_energy:.1f} kWh")
+    st.write(f"Batt Disharge: {batt_discharge_energy:.1f} kWh")
+    st.write(f"Grid export: {grid_export_energy:.1f} kWh")
+    st.write(f"Total efficiency: {(output_energy-grid_import_energy)/pv_energy*100:.1f} %")
+    
+    st.write(day_energy)
+
     # tu możesz dodać przetwarzanie, wykresy itd.
-    st.write("Wykres (przykładowy):")
-    fig, ax = plt.subplots()
-    df["Output Active Power(W)"].plot(ax=ax)
-    st.pyplot(fig)
+    #st.write("Wykres (przykładowy):")
+    #fig, ax = plt.subplots()
+    #df["Output Active Power(W)"].plot(ax=ax)
+    #st.pyplot(fig)
